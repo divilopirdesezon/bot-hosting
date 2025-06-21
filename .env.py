bfe008eb4bd3e9393c2bee213fa81f8da6d099bd
divilopirdesezon <carnatel20@gmail.com>
2025-06-22 00:12:38 +0300

    Add files via upload

---
+++ .env.py
@@ -0,0 +1,8 @@
+DISCORD_TOKEN=token
+GUILD_ID=server id
+NOTIFY_ROLE_ID=un id, de preferat, unu cu administrator
+SUGGESTIONS_CHANNEL_ID=canal de sugestii
+ROLE_ID_1=role id pentru diferite accese
+ROLE_ID_2=role id pentru diferite accese
+CATEGORY_ID=id categorie tickete
+SUPPORT_ROLE_ID=id support 
